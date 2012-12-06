a048a0e348dd6c3d4e84dacecb745ea66e8c68ff
Guillaume Hautbois <guillaume.hautbois@gmail.com>
2012-12-06 09:53:14 +0100

    Unique key was blocking in some cases

--- install-dev/upgrade/sql/1.5.2.1.sql
+++ install-dev/upgrade/sql/1.5.2.1.sql
@@ -5,3 +5,6 @@ ALTER TABLE `PREFIX_address` CHANGE  `outstanding_allow_amount` `outstanding_all
 /* PHP:blocknewsletter1530(); */;
 
 /* PHP:block_category_1521(); */;
+
+ALTER TABLE  `prestashop15`.`ps_stock_available` DROP INDEX  `id_product_2` ,
+ADD UNIQUE  `id_product_2` (  `id_product` ,  `id_product_attribute` ,  `id_shop` ,  `id_shop_group` );
